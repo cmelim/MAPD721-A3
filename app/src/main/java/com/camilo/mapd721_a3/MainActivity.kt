168ab133a1ddb878916ec101508468ce877d2228
Camilo Meli <camilo.meli@gmail.com>
2024-03-16 12:04:03 -0400

    Add navigation config

--- app/src/main/java/com/camilo/mapd721_a3/MainActivity.kt
+++ app/src/main/java/com/camilo/mapd721_a3/MainActivity.kt
@@ -3,13 +3,20 @@ package com.camilo.mapd721_a3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.camilo.mapd721_a3.ui.theme.MAPD721A3Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,15 +24,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MAPD721A3Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AppNavigation()
             }
         }
+    }
+}
+@Composable
+fun MainScreen(navController: NavController) {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Button(onClick = { navController.navigate("screen1") }) {
+            Text("Go to Screen 1")
+        }
+        Button(onClick = { navController.navigate("screen2") }) {
+            Text("Go to Screen 2")
+        }
+        Button(onClick = { navController.navigate("screen3") }) {
+            Text("Go to Screen 3")
+        }
+        Button(onClick = { navController.navigate("screen4") }) {
+            Text("Go to Screen 4")
+        }
+
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MainScreen(navController) }
+        composable("screen1") { Screen1() }
+        composable("screen2") { Screen2() }
+        composable("screen3") { Screen3() }
+        composable("screen4") { Screen4() }
     }
 }
 
